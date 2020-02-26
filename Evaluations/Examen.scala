188f3b17c0672c202d285e2fb02bfb6a696e2f5a
Armando <jesus.aguirre@tectijuana.edu.mx>
2020-02-25 17:37:20 -0800

    se modifico la rama unidad 1

--- Evaluations/Examen.scala
+++ Evaluations/Examen.scala
@@ -24,4 +24,8 @@ def diagonalDifference(arr:Array[Array[Int]]): Int= {
     return DiffAbs
 }
 
-print(diagonalDifference(Array(Array(11,2,4),Array(4,5,6),Array(10,8,-12))))
+var arr=Array(Array(11,2,4),Array(4,5,6),Array(10,8,-12))
+
+
+diagonalDifference(arr)
+
