c896b1ebc26f46805a43b0f8cc8fba90fc228e83
Armando <jesus.aguirre@tectijuana.edu.mx>
2020-03-03 14:58:43 -0800

    Comments were added to practice 6

--- Practices/Practice 6/Querys_Diamonds.scala
+++ Practices/Practice 6/Querys_Diamonds.scala
@@ -6,35 +6,45 @@ val df = spark.read.option("header", "true").option("inferSchema","true")csv("/h
 
 df.printSchema()
 
-
 import spark.implicits._
-
 
 df.show()
 
-//scala notation
+//SPARKSQL NOTATION
+
+// Returns the columns with prices greater than 350.
 df.filter($"price">350).show()
 
-//sparksql notation
+// Returns the columns with prices greater than 480.
 df.filter("price> 480").show()
 
+// Returns the columns with the price less than 340 and the carat less than 0.30
 df.filter($"price" < 340 && $"carat" < 0.30).show()
 
+// Returns the columns with the price less than 340 and the carat less than 0.30
 df.filter("price < 340 AND carat < 0.30").show()
 
-//Collect the results
+// Collect the results
 val result = df.filter($"price" < 340 && $"carat" < 0.30).collect()
 
-//Count the results
+// Count the results
 val result = df.filter($"[price" < 340 && $"carat" < 0.30).count()
 
-//equality
+// Equality
+// Returns the column when the clarity is equal to SI1
 df.filter($"clarity"==="SI1").show()
+
+// Returns the Clarity number grouped by Clarity.
 df.groupBy("clarity").count().show()
 
+// Count the different clarity.
 df.select(countDistinct("clarity")).show()
+
+// Calculate the average the carat column.
 df.select(avg("carat")).show()
+
+// Calculate the average the price column.
 df.select(avg("price")).show()
+
+// Returns all columns order by clarity.
 df.orderBy("clarity").show()
-
-
