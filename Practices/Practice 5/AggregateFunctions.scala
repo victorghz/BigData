5db7fd82b2acf37746d8712964e320e32f27a560
Armando <jesus.aguirre@tectijuana.edu.mx>
2020-03-03 13:36:26 -0800

    Added comments on practice 5

--- Practices/Practice 5/AggregateFunctions.scala
+++ Practices/Practice 5/AggregateFunctions.scala
@@ -7,62 +7,62 @@ import spark.implicits._
 
 df.printSchema()
 
-// [ 1 ]
+// [ 1 ] Returns the approximate number of distinct items in a group.
 df.select(approx_count_distinct("Date": String)).show()    
 
-// [ 2 ]
+// [ 2 ] Returns the average of the values in a group.
 df.select(avg("Volume")).show()
 
-// [ 3 ]
+// [ 3 ] Returns a list of objects with duplicates.
 df.select(collect_list("Date")).show()
 
-// [ 4 ]
+// [ 4 ] Returns a set of objects with duplicate elements eliminated.
 df.select(collect_set("Open")).show()
 
-// [ 5 ]
+// [ 5 ] Returns the Pearson Correlation Coefficient for two columns.
 df.select(corr("Open","Volume")).show()
 
-// [ 6 ]
+// [ 6 ] Returns the number of items in a group.
 df.select(count("Volume")).show()
 
-// [ 7 ]
+// [ 7 ] Returns the number of distinct items in a group.
 df.select(countDistinct("Date")).show()
 
-// [ 8 ]
+// [ 8 ] Returns the population covariance for two columns.
 df.select(covar_pop("High","Low")).show()
 
-// [ 9 ]
+// [ 9 ] Returns the sample covariance for two columns.
 df.select(covar_samp("High","Low")).show()
 
-// [ 10 ]
+// [ 10 ] Returns the first value of a column in a group.
 df.select(first("Date")).show()
 
-// [ 11 ]
+// [ 11 ] Returns the kurtosis of the values in a group.
 df.select(kurtosis("Low")).show()
 
-// [ 12 ]
+// [ 12 ] Returns the last value of the column in a group.
 df.select(last("Date")).show()
 
-// [ 13 ]
+// [ 13 ] Returns the maximum value of the column in a group.
 df.select(max("Date")).show()
 
-// [ 14 ]
+// [ 14 ] Returns the average of the values in a group.
 df.select(mean("Date")).show()
 
-// [ 15 ]
+// [ 15 ] Returns the minimum value of the column in a group.
 df.select(min("Date")).show()
 
-// [ 16 ]
+// [ 16 ] Returns the skewness of the values in a group.
 df.select(skewness("Date")).show()
 
-// [ 17 ]
+// [ 17 ] alias for stddev_samp.
 df.select(stddev("Close")).show()
 
-// [ 18 ]
+// [ 18 ] Returns the population standard deviation of the expression in a group.
 df.select(stddev_pop("Volume")).show()
 
-// [ 19 ]
+// [ 19 ] Returns the sample standard deviation of the expression in a group.
 df.select(stddev_samp("Volume")).show()
 
-// [ 20 ]
-df.select(stddev_samp("Volume")).show()
+// [ 20 ] Returns the population variance of the values in a group. 
+df.select(var_pop("Volume")).show()
